04c6234accde64480de891447d3d5e2c56a3fbca
jstamn <jstamn@wisc.edu>
2018-03-17 12:29:34 -0500

    created is_Empty and set constructor

--- p3/src/HashTable.java
+++ p3/src/HashTable.java
@@ -1,47 +1,54 @@
-//Testing commit
-
-
-
-
-
 
 public class HashTable<K, V> implements HashTableADT<K, V> {
-    /* Instance variables and constructors
+    /*
+     * Instance variables and constructors
      */
+    double loadFactor = 0.75;
+    V[] hashTable;
+    int arraySize;
+    int itemCount = 0;
 
     @Override
     public V put(K key, V value) {
-        //TODO: Implement put method - using efficient algorithm
-    	
+        // TODO: Implement put method - using efficient algorithm
+
         return null;
+    }
+    
+    private K hashFunction(V value) {
+        K key = null;
+        return key;
     }
 
     @Override
     public void clear() {
-       //TODO: Implement this method
+        // TODO: Implement this method
     }
 
     @Override
     public V get(K key) {
-        //TODO: Implement the get method
+        // TODO: Implement the get method
         return null;
     }
 
     @Override
     public boolean isEmpty() {
-        //TODO: Implement the method
-        return true;
+        // TODO: Implement the method
+        if (itemCount == 0) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public V remove(K key) {
-       //TODO: Implement the remove method
+        // TODO: Implement the remove method
         return null;
     }
 
     @Override
     public int size() {
-        //TODO: Implement this method
+        // TODO: Implement this method
         return 0;
     }
 }
