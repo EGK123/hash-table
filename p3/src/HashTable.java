4020813effe4aa82d55415c787fe265e2cdd9e95
Ege Gurcan Kula <ekula@wisc.edu>
2018-03-18 12:43:36 -0500

    almost completed array increaser

--- p3/src/HashTable.java
+++ p3/src/HashTable.java
@@ -1,91 +1,131 @@
 import java.util.NoSuchElementException;
 
 public class HashTable<K, V> implements HashTableADT<K, V> {
-    /*
-     * Instance variables and constructors
-     */
-    double loadFactor = 0.75;
-    int arraySize;
-    V[] hashTable;
-    int itemCount = 0;
+	/*
+	 * Instance variables and constructors
+	 */
+	double loadFactor = 0.75;
+	int arraySize;
+	V[] hashTable;
+	K[] keys;
+	int itemCount = 0;
 
-    /**
-    *
-    * @param key : The key that goes into the hashtable
-    * @param value: The Value associated with the key
-    * @return value of the key added to the hashtable,
-    *      throws NullPointerException if key is null
-    */
-    @Override
-    public V put(K key, V value) {
-        // TODO: Implement put method - using efficient algorithm
-        if (key == null) {
-            throw new NullPointerException();
-        }
-        V old = null;
-        int index = hashFunction(key);
-        if (hashTable[index] == null) {
-            old = hashTable[index];
-            hashTable[index] = value;
-        } else {
-            boolean insert = false;
-            while (insert == false) {
-                index += 1;
-                if (hashTable[index] == null) {
-                    old = hashTable[index];
-                    hashTable[index] = value;
-                    insert = true;
-                }
-            }
-        }
-        return old;
-    }
-    
-    private int hashFunction(K key) {
-        //FIXME: takes wrong parameter, must take key and output hashIndex
-        
-        return -99;
-    }
+	HashTable(int size) {
+		arraySize = size;
+		hashTable = (V[]) new Object[size];
+		keys = (K[]) new Object[size];
+	}
 
-    @Override
-    public void clear() {
-        // TODO: Implement this method
-    }
+	/**
+	 *
+	 * @param key
+	 *            : The key that goes into the hashtable
+	 * @param value:
+	 *            The Value associated with the key
+	 * @return value of the key added to the hashtable, throws NullPointerException
+	 *         if key is null
+	 */
+	@Override
+	public V put(K key, V value) {
+		// TODO: Implement put method - using efficient algorithm
+		if (key == null) {
+			throw new NullPointerException();
+		}
+		V old = null;
+		int index = hashFunction(key);
+		if (hashTable[index] == null) {
+			old = hashTable[index];
+			hashTable[index] = value;
+		} else {
+			boolean insert = false;
+			while (insert == false) {
+				index += 1;
+				if (hashTable[index] == null) {
+					old = hashTable[index];
+					hashTable[index] = value;
+					insert = true;
+				}
+			}
+		}
+		return old;
+	}
 
-    /**
-     * @param key: The key for which the value is returned
-     * @return The value associated with the key,
-     *          else throws NoSuch Element Exception
-     */
-    @Override
-    public V get(K key) {
-        // TODO: Implement the get method
-        int index = hashFunction(key);
-        V value = hashTable[index];
-        if (value == null) {
-            throw new NoSuchElementException();
-        }
-        return value;
-    }
+	private int hashFunction(K key) {
+		// FIXME: takes wrong parameter, must take key and output hashIndex
 
-    @Override
-    public boolean isEmpty() {
-        // TODO: Implement the method
-        if (itemCount == 0) {
-            return true;
-        }
-        return false;
-    }
+		return -99;
+	}
 
-    @Override
-    public V remove(K key) {
-        // TODO: Implement the remove method
-        return null;
-    }
+	@Override
+	public void clear() {
+		// TODO: Implement this method
+	}
 
-    @Override
-    public int size() {
-        // TODO: Implement this method
-        return 0;
-    }
+	/**
+	 * @param key:
+	 *            The key for which the value is returned
+	 * @return The value associated with the key, else throws NoSuch Element
+	 *         Exception
+	 */
+	@Override
+	public V get(K key) {
+		// TODO: Implement the get method
+		int index = hashFunction(key);
+		V value = hashTable[index];
+		if (value == null) {
+			throw new NoSuchElementException();
+		}
+		return value;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void increase(int currentArraySize) {
+		boolean prime = false;
+		int nextPrime = 0;
+		int num = currentArraySize;
+		for (num++; !prime; num++) {
+
+			if (!(num % 2 == 0)) {
+				prime = true;
+				for (int i = 3; i * i <= num; i += 2) {
+					if (!(num % i == 0) && prime) {
+						prime = true;
+						nextPrime = num;
+					} else
+						prime = false;
+
+				}
+			}
+
+		}
+		V[] tempArray = hashTable ;
+		int tempSize = arraySize;
+		hashTable = (V[]) new Object[nextPrime];
+		int j = hashTable.get(12);
+		arraySize = nextPrime;
+		for(int i=0;i<itemCount;i++) {
+			hashTable.put(keys[i], hashTable.get(keys[i]) );
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO: Implement the method
+		if (itemCount == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO: Implement the remove method
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO: Implement this method
+		return 0;
+	}
 }
