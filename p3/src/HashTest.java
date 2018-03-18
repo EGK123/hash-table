478d3b46e3e0a70d042bfe2660c5a9e62e06bf43
jstamn <jstamn@wisc.edu>
2018-03-18 15:09:36 -0500

    created HashTest

--- p3/src/HashTest.java
+++ p3/src/HashTest.java
@@ -8,4 +8,44 @@ import org.junit.Test;
 
 public class HashTest {
 
+	HashTableADT<Integer, String> table = null;
+	String expected = null;
+	String actual = null;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		table = new HashTable(11, .75);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test01_one_insert_null_key() {
+		expected = "true";
+		actual = "" + table.put(null, "value");
+		if (! expected.equals(actual))
+			fail("expected: "+expected+ " actual: "+actual);
+	}
 }
